a5fe05178b1c7e429da86d3066190b74176ad8e8
Уразов Андрей <regandess@gmail.com>
2025-09-03 20:11:25 +0300

    start project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Main} from './views/main/main';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
